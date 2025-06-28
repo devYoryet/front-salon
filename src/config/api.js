f5f63e54c0212334f808c4508dc976ccbd7ae032
yoryet <y.danoun@pharmatender.cl>
2025-06-27 21:33:37 -0400

    feat : agregar el puerto 8081 del backoffice

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 // const DEPLOYED=''
-const LOCALHOST='http://localhost:5000'
+const LOCALHOST = 'http://localhost:8081'  // Cambiar puerto
 
 export const API_BASE_URL = LOCALHOST
 
